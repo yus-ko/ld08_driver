eeefdb78e590b1bc9b4612ef0199aaa00e2d33a4
ashekim <ashekim@robotis.com>
2022-05-02 17:14:13 +0900

    modified launch file

--- launch/ld08.launch.py
+++ launch/ld08.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='ld08_driver',
-            executable='ld08_driver',
+            node_executable='ld08_driver',
             name='ld08_driver',
             output='screen'),
     ])
